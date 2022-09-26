0f2d8ef8d2d2401625a0a7f1f69a85b923d6c092
GouthamGroza <gouthamkumar445@gmailcom>
2022-09-26 08:01:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { Ng2TelInputModule } from 'ng2-tel-input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContentComponent,
+    HomepageComponent,
+    RegisterPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2TelInputModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginPageComponent},
+      {path: 'registerPage', component: RegisterPageComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
